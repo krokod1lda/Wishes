a23f9db625cd767b79aabb8348cec1b74327fee5
Alexandra <16yungsanches@gmail.com>
2023-09-21 12:34:01 +0300

    Initial commit

---
+++ src/main/java/com/krokod1lda/wishes/WishesApplication.java
@@ -0,0 +1,13 @@
+package com.krokod1lda.wishes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WishesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/krokod1lda/wishes/WishesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krokod1lda.wishes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
